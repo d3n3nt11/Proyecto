c40a01fcf0a3f3eacd639c0c025fbaed5625afc8
Galverr052 <galverr052@g.educaand.es>
2026-03-02 13:37:53 +0100

    Creación de proyecto en spring-boot

---
+++ fuegoypan/src/main/java/com/example/fuegoypan/FuegoYPanTfgApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuegoypan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuegoYPanTfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuegoYPanTfgApplication.class, args);
+	}
+
+}
---
+++ fuegoypan/src/test/java/com/example/fuegoypan/FuegoYPanTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuegoypan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuegoYPanTfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
